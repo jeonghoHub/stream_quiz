47f3ac10b67593cddaa41c63a08615595af2c1ab
jeonghoHub <tjsrsdqq4@gmail.com>
2022-05-22 19:28:08 +0900

    Feat: quiz3 success

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -1,6 +1,7 @@
 package com.mangkyu.stream.Quiz1;
 
 import com.opencsv.CSVReader;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,9 +10,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.*;
 import static java.util.function.Function.*;
+import static java.util.stream.Collectors.*;
 import static java.util.stream.Collectors.toMap;
 
 public class Quiz1 {
@@ -33,10 +36,27 @@ public class Quiz1 {
                 .collect(toMap(identity(), value -> 1, Integer::sum));
     }
 
+
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
+        return csvLines.stream()
+                .map(strings -> strings[2])
+                .mapToInt(line -> func(line, "좋아", 0))
+                .sum();
+    }
+
+    private int func(@NotNull String value, String target, int index) {
+        int resultInt = value.indexOf(target, index);
+        return stopRecursionIfTargetNotFound(value, target, resultInt);
+    }
+
+    private int stopRecursionIfTargetNotFound(String value, String target, int resultInt) {
+        if(resultInt > 0) {
+            return 1 + func(value, target, resultInt + 2);
+        }
         return 0;
     }
+
 
     private List<String[]> readCsvLines() throws IOException {
         CSVReader csvReader = new CSVReader(new FileReader(getClass().getResource("/user.csv").getFile()));
